9e8a127addefdeaa1a5a82d04bc376a0eea5d20c
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-02-01 13:51:12 +0900

    feat:add another avatar choose

--- src/components/ChooseA.vue
+++ src/components/ChooseA.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="flex justify-center">
-    <div class="grid place-items-center grid-cols-1 grid-rows-3">
-      <div class="border-white border-4 my-5 p-3 bg-sky-200">
-        <div class="bg-transparent text-3xl text-red-600 font-bold underline">
-          CHOOSING
-        </div>
-      </div>
+  <div class="">
+    <div class="flex justify-center">
       <div class="grid grid-cols-3 grid-rows-1 gap-8 bg-white row-span-2">
         <div
           class="border-yellow-200 border-4 mr-4 p-4 bg-transparent text-3xl text-red-600 font-bold underline"
@@ -23,6 +18,14 @@
           Avatar 3
         </button>
       </div>
+    </div>
+    <div class="grid place-items-center grid-cols-1 grid-rows-3">
+      <div class="border-white border-4 my-5 p-3 bg-sky-200">
+        <div class="bg-transparent text-3xl text-red-600 font-bold underline">
+          CHOOSING
+        </div>
+      </div>
+
       <div class="border-white border-4 my-5 p-3 bg-sky-200">
         <button
           v-on:click="play"
@@ -37,6 +40,25 @@
           class="bg-transparent text-3xl text-red-600 font-bold underline"
         >
           Play a fucking sound (Real fucking sound)
+        </button>
+      </div>
+    </div>
+    <div class="flex justify-center mt-20">
+      <div class="grid grid-cols-3 grid-rows-1 gap-8 bg-white row-span-2">
+        <div
+          class="border-yellow-200 border-4 mr-4 p-4 bg-transparent text-3xl text-red-600 font-bold underline"
+        >
+          Avatar 1
+        </div>
+        <div
+          class="border-yellow-200 border-4 mx-4 p-4 bg-transparent text-3xl text-red-600 font-bold underline"
+        >
+          Avatar 2
+        </div>
+        <button
+          class="border-yellow-200 border-4 ml-4 p-4 bg-transparent text-3xl text-red-600 font-bold underline"
+        >
+          Avatar 3
         </button>
       </div>
     </div>
