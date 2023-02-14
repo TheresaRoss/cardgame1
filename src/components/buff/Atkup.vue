51f1eb21b4902f049d983322b9e3e174d8220ac9
Thut Thamrongnawasawat <thutthamrongnawasawat@Thuts-MacBook-Air.local>
2023-02-14 16:05:32 +0700

    feat:add first icon for buff

--- src/components/buff/Atkup.vue
+++ src/components/buff/Atkup.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-5 h-5 bg-green-300 flex items-center justify-center">
+  <div class="w-5 h-5 bg-transparent flex items-center justify-center">
+    <img :src="'src/assets/picture/buff/atkup.png'" alt="atkup" />
     <div class="bg-transparent text-black">{{ duration }}</div>
   </div>
 </template>
